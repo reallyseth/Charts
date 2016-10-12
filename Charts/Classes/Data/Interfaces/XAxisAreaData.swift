9577c19dea21d48a69b5893ea5119b3dea71c77c
Seth Hein <me@reallyseth.com>
2016-10-12 15:55:30 -0500

    Created XAxisAreaData

---
+++ Charts/Classes/Data/Interfaces/XAxisAreaData.swift
@@ -0,0 +1,17 @@
+//
+//  ChartXAxisAreaData.swift
+//  Pods
+//
+//  Created by Richard Wagner on 1/11/16.
+//
+//
+
+import UIKit
+
+public class ChartXAxisAreaData: NSObject {
+    
+    public var startX = CGFloat(0)
+    public var endX = CGFloat(0)
+    public var color = UIColor(red:215/255, green: 231/255, blue: 241/255, alpha: 0.5)
+    
+}
